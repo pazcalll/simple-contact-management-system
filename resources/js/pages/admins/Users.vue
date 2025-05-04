1a3136d1fb9b37450be1b6b0c41a334865c98f89
pazcalll <yazeedarifin@gmail.com>
2025-05-05 03:10:37 +0700

    feat: vue with table

---
+++ resources/js/pages/admins/Users.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { ref, reactive } from 'vue'
+
+</script>
+
+<template>
+    <AppLayout>
+        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+            <div class="grid auto-rows-min gap-4 md:grid-cols-3">
+                <!-- <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                    <PlaceholderPattern />
+                </div>
+                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                    <PlaceholderPattern />
+                </div>
+                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                    <PlaceholderPattern />
+                </div> -->
+            </div>
+            <div class="relative min-h-[100vh] flex-1 md:min-h-min">
+                <div class="border rounded-lg w-full">
+                    <Table class="w-full">
+                        <TableHeader class="text-[12pt]">
+                            <TableRow class="bg-[#F5F5F4] dark:bg-[#1C1C1A]">
+                                <TableHead>Name</TableHead>
+                                <TableHead>Email</TableHead>
+                                <TableHead>Role</TableHead>
+                                <TableHead>Mobile</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell>John</TableCell>
+                                <TableCell>Doe</TableCell>
+                                <TableCell>Admin</TableCell>
+                                <TableCell>Admin</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Role;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\Admin;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
@@ -10,6 +12,10 @@ Route::get('/', function () {
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::prefix('admins')->middleware(['role:'.Role::ROLE_ADMIN])->group(function () {
+    Route::resource('users', Admin\UserController::class);
+});
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
